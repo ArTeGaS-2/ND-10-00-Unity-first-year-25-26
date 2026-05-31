c2c3266b3c7d780b692a5f568a62a86615f88a22
ArTeGaS-2 <probablytorvik@gmail.com>
2026-05-31 20:56:13 +0300

    Додали підготовку стрибка гравця

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Переміщення")]
     public float speed = 10f; // Швидкість руху гравця
+    public float maxSpeed = 8f;
+    public float jumpForce = 7f;
+
+    [Header("GroundCheck")]
+    public Transform groundCheck; // Точка для перевірки, чи знаходиться гравець на землі
+    public float groundCheckRadius = 0.25f; // Радіус для перевірки, чи знаходиться гравець на землі
+    public LayerMask groundLayer; // Шар, який визначає, що є землею
 
     private Rigidbody rb; // Фізичний компонент
+    private bool isGrounded; // Чи на землі ми
 
     private void Start() // Викликається при запуску гри
     {
         // Отримуємо компонент Rigidbody
         rb = GetComponent<Rigidbody>();
     }
-
     // Викликається з фіксованою частотою, використовується для фізичних обчислень
     private void FixedUpdate() 
+    {
+        Move();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(
+            rb.velocity.x,
+            0f,
+            rb.velocity.z);
+
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    private void Move()
     {
         float horizontal = Input.GetAxis("Horizontal"); // Вісь руху вправо/вліво
         float vertical = Input.GetAxis("Vertical"); // Вісь руху вперед/назад
@@ -24,11 +52,11 @@ public class Player : MonoBehaviour
 
         rb.velocity = move * speed;
     }
-    private void Update()
+    private void GroundCheck()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddForce(0, 5, 0, ForceMode.Impulse);
-        }
+        isGrounded = Physics.CheckSphere(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer);
     }
 }
